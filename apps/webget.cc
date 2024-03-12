1b6fcc2a5541e3a88e1a3bf9d1112cb135bbd716
yyym-y <yinmingyang89@gmail.com>
2024-03-12 23:17:42 +0800

    build envir and finish get_URL()[All pass]

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,31 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  cerr << "Your Host is: " << host << ", visit path is : " << path << "\n";
+  // find the host address
+  Address hostAddress = Address(host, "http");
+  cerr << "building socket ...\n";
+  // try to build a TCP socket
+  TCPSocket socket = TCPSocket();
+  socket.connect(hostAddress); // connect to host address
+  cerr << "local_address: " << socket.local_address().to_string() <<
+          "\npeer_address: " << socket.peer_address().to_string() << "\n";
+  // begin to write
+  string str1 = "GET " + path + " HTTP/1.1\r\n";
+  string str2 = "Host: " + host + "\r\n";
+  string str3 = "Connection: close\r\n";
+  string end = "\r\n";
+  socket.write(str1);
+  socket.write(str2);
+  socket.write(str3);
+  socket.write(end);
+  // begin to read
+  string output;
+  while (! socket.eof()) {
+    socket.read(output);
+    cout << output;
+  }
+  socket.close();
 }
 
 int main( int argc, char* argv[] )
