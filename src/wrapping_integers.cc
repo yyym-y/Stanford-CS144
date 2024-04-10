6374632d1f1848e2a859fe0fe3e282ae709080b4
yyym-y <yinmingyang89@gmail.com>
2024-04-10 19:58:44 +0800

    finish lab2 partI

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,26 @@
 #include "wrapping_integers.hh"
+#include <iostream>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  uint32_t num = static_cast<uint32_t>(n);
+  return zero_point + num;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t top_32 = checkpoint >> 32;
+  uint64_t low_32 = checkpoint << 32 >> 32;
+  uint64_t num = static_cast<uint64_t>(this->raw_value_ - zero_point.get_raw_value());
+  int bias_r = num >= low_32 ? 0 : 1;
+  int bias_l = num <= low_32 ? 0 : 1;
+  uint64_t larger =  static_cast<uint64_t>(num + (static_cast<uint64_t>(top_32 + bias_r) << 32));
+  uint64_t less = static_cast<uint64_t>(num + (static_cast<uint64_t>(top_32 - bias_l) << 32));
+  if(top_32 != 0)
+    return larger - checkpoint > checkpoint - less ? less : larger;
+  if(num <= checkpoint)
+    return larger - checkpoint > checkpoint - num ? num : larger;
+  return larger;
 }
--- src/wrapping_integers.hh
+++ src/wrapping_integers.hh
@@ -27,6 +27,8 @@ public:
 
   Wrap32 operator+( uint32_t n ) const { return Wrap32 { raw_value_ + n }; }
   bool operator==( const Wrap32& other ) const { return raw_value_ == other.raw_value_; }
+  bool operator<( uint32_t other ) const { return raw_value_ < other; }
+  uint32_t get_raw_value() const {return raw_value_;}
 
 protected:
   uint32_t raw_value_ {};
